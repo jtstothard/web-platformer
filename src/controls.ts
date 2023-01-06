088f51b1c1a9420798cb08b0a2b0b529cba72dc3
Jay Stothard <jtstothard@gmail.com>
2023-01-06 13:38:29 +0000

    debounce left right

--- src/controls.ts
+++ src/controls.ts
@@ -4,6 +4,7 @@ export type DirectionType = typeof directions[number];
 
 export class Controls {
   keys: string[];
+  currentKeys: string[] = [];
 
   public keyMap: {
     [key: string]: string;
@@ -42,6 +43,7 @@ export class Controls {
 
   // handle key down event
   private handleKeyDown(event: KeyboardEvent) {
+    this.currentKeys.push(event.key);
     const direction = this.keys.find((key) => event.key === this.keyMap[key]);
 
     if (direction) {
@@ -51,11 +53,17 @@ export class Controls {
 
   // handle key up event
   private handleKeyUp(event: KeyboardEvent) {
+    // debounce left and right keys
     const isLeftOrRight =
       event.key === this.keyMap.left || event.key === this.keyMap.right;
 
-    if (isLeftOrRight) {
+    const isLeftAndRightPressed =
+      this.currentKeys.includes(this.keyMap.left) &&
+      this.currentKeys.includes(this.keyMap.right);
+
+    if (isLeftOrRight && !isLeftAndRightPressed) {
       this.update('stop');
     }
+    this.currentKeys = this.currentKeys.filter((key) => key !== event.key);
   }
 }
